42d038eed94f3c405d122db375fba5761905ad74
Seungkyu-Han <trust1204@gmail.com>
2025-07-06 12:35:47 +0900

    docs: add mysql config in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '1204',
+    database: 'usedCars',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true
+  }), UsersModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
