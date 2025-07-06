f0ec7e24629662c19fa80b72dc8e3b6cb01c8cf0
Seungkyu-Han <trust1204@gmail.com>
2025-07-06 22:55:50 +0900

    feat: register user entity in user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import {Users} from "./users.entity";
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Users])],
   controllers: [UsersController],
   providers: [UsersService]
 })
