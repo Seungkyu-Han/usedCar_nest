3b868633c7ae69aec6081d68749fb2766488e8bf
Seungkyu-Han <trust1204@gmail.com>
2025-07-08 21:38:42 +0900

    feat: user dto interceptor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,19 @@
-import {Body, Controller, Delete, Get, Param, Patch, Post, Query} from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+    Query,
+} from '@nestjs/common';
 import {UsersService} from "./users.service";
 import {CreateUserDto} from "./dto/create-user.dto";
 import {UpdateUserDto} from "./dto/update-user.dto";
 import {Users} from "./users.entity";
+import {Serialize} from "../interceptors/serialize.interceptor";
+import {UserDto} from "./dto/user.dto";
 
 @Controller('users')
 export class UsersController {
@@ -15,11 +26,13 @@ export class UsersController {
     }
 
     @Get("/:id")
+    @Serialize(UserDto)
     findUser(@Param('id') id: number): Promise<Users> {
         return this.usersService.findOne(id)
     }
 
     @Get()
+    @Serialize(UserDto)
     findAllUsers(@Query() name: string): Promise<Users[]> {
         return this.usersService.findAll(name);
     }
