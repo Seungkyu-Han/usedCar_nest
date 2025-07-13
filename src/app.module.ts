43a86c8a9f37124f4e588ca72efd78415493530c
Seungkyu-Han <trust1204@gmail.com>
2025-07-13 14:19:10 +0900

    feat: config db environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,30 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import {ConfigModule, ConfigService} from "@nestjs/config";
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: '1204',
-    database: 'usedCars',
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    synchronize: true
-  }), UsersModule, ReportsModule],
+  imports: [
+      ConfigModule.forRoot({
+        isGlobal: true,
+        envFilePath: `.env.${process.env.NODE_ENV}`
+      }),
+      TypeOrmModule.forRootAsync({
+        inject: [ConfigService],
+        useFactory: (config: ConfigService) => {
+          return {
+            type: "mysql",
+            host: config.get<string>('DB_HOST'),
+            port: config.get<number>('DB_PORT'),
+            username: config.get<string>('DB_USERNAME'),
+            password: config.get<string>('DB_PASSWORD'),
+            database: config.get<string>('DB_DATABASE'),
+            entities: [__dirname + '/**/*.entity{.ts,.js}'],
+            synchronize: true
+          }
+        }
+      }),
+    UsersModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
